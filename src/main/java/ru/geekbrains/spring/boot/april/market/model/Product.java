42462c37673c302e90af7e3ee1803f64de77a8e5
Lina Rusakova <xlinaris@gmail.com>
2021-05-03 19:06:33 +0300

    2 commit April Market

--- src/main/java/ru/geekbrains/spring/boot/april/market/model/Product.java
+++ src/main/java/ru/geekbrains/spring/boot/april/market/model/Product.java
@@ -1,4 +1,18 @@
 package ru.geekbrains.spring.boot.april.market.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table("products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column("id")
+    private Long id;
+
+
 }
